bd0d269b4a641575adea283043125b5f063c58eb
Osman-Obedmot <obedosman2000@gmail.com>
2026-01-06 16:44:41 +0300

    updated order tests

--- __test__/order.test.js
+++ __test__/order.test.js
@@ -17,7 +17,7 @@ describe("Order endpoint tests", () => {
   const testOrder = {
     products: [
       {
-        productName: "iPhone 14",
+        productName: "iPhone 14 Pro Max",
         quantity: 1,
       },
     ],
