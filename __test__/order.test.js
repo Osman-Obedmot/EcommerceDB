9f79a536cef91fb4338ab5bdaa5f673f191b3e37
Osman-Obedmot <obedosman2000@gmail.com>
2026-01-06 16:10:26 +0300

    updated product and order tests by comment the delete code

--- __test__/order.test.js
+++ __test__/order.test.js
@@ -19,7 +19,7 @@ describe("Order endpoint tests", () => {
   const testOrder = {
     products: [
       {
-        productName: "iPhone 14 Pro Max",
+        productName: "iPhone 14",
         quantity: 1,
       },
       // {
